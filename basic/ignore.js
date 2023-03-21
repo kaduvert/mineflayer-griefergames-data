1befa83b37a464344db73e29ef2c5fb1984f0d7a
KadaverBrutalo10 <jimmybiel123@gmail.com>
2023-03-21 14:39:03 +0100

    moved ignore into own file

--- basic/ignore.js
+++ basic/ignore.js
@@ -1,6 +1,12 @@
 module.exports = {
-    add: /^Du ignorierst ab jetzt Spieler (\S+)\.$/,
-	remove: /^Du ignorierst Spieler (\S+) nicht mehr\.$/,
-	list: /^Ignoriert: (.+)$/,
-	listEmpty: /^Du ignoriest niemanden\.$/
+    chatPatterns: {
+        add: /^Du ignorierst ab jetzt Spieler (\S+)\.$/,
+        remove: /^Du ignorierst Spieler (\S+) nicht mehr\.$/,
+        list: /^Ignoriert: (.+)$/,
+        listEmpty: /^Du ignoriest niemanden\.$/
+    },
+    commands: {
+		list: IGNORE_COMMAND_PREFIX,
+		target: IGNORE_COMMAND_PREFIX + '$1',
+    }
 }
--- basic/privateChat.js
+++ basic/privateChat.js
@@ -11,8 +11,6 @@ module.exports = {
 	},
 	commands: {
 		send: '/msg $1 $2',
-		ignoredList: IGNORE_COMMAND_PREFIX,
-		ignore: IGNORE_COMMAND_PREFIX + '$1',
 		toggle: '/msgtoggle',
 	}
 }
