4eb898cdb0656ddcd0a3e968d87b5c8b5cde6f90
KadaverBrutalo10 <jimmybiel123@gmail.com>
2023-03-31 17:45:17 +0200

    fixed bank chatPatterns

--- advanced/bank.js
+++ advanced/bank.js
@@ -3,8 +3,8 @@ const COMMAND_PREFIX = '/bank '
 module.exports = {
     chatPatterns: {
         balance: /^\[Bank\] Kontostand: (\d+)$/,
-        depositSuccess: /^\[Bank\] Du hast 2000 auf dein Bankkonto eingezahlt\.$/,
-        withdrawSuccess: /^\[Bank\] Du hast 2999 von deinem Bankkonto abgehoben\.$/,
+        depositSuccess: /^\[Bank\] Du hast (\d+) auf dein Bankkonto eingezahlt\.$/,
+        withdrawSuccess: /^\[Bank\] Du hast (\d+) von deinem Bankkonto abgehoben\.$/,
         help: /^\[Bank\] Die Bank sichert dein Geld vor Verlust durch Serverfehler oder einem Reset\.$/,
 
         invalidNumberError: /^\[Bank\] (.+) ist keine gültige Zahl\.$/,
