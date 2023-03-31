c8a92dd5789459505f7e00c4c418e84d4c11816d
KadaverBrutalo10 <jimmybiel123@gmail.com>
2023-03-31 12:25:02 +0200

    added pattern alias logic

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const regexAliases = {
+    '\\$GG': '\\[GrieferGames\\]'
+}
+
+function transformPattern(regex) {
+    let regexString = regex.toString()
+    regexString = regexString.substring(1, regexString.length - 1)
+    Object.keys(regexAliases).forEach(alias => {
+        regexString = regexString.replace(new RegExp(alias, 'g'), regexAliases[alias])
+    })
+    return new RegExp(regexString)
+}
+
 function getAllData() {
     const branches = [
         require('./basic'),
@@ -9,6 +22,19 @@ function getAllData() {
     branches.forEach(modules => {
         Object.keys(modules).forEach(moduleName => {
             returnData[moduleName] = modules[moduleName]
+
+            // pattern alias logic
+            const chatPatterns = returnData[moduleName].chatPatterns
+            if (Object.keys(chatPatterns).length) {
+                Object.keys(chatPatterns).forEach(patternKey => {
+                    const chatPattern = chatPatterns[patternKey]
+                    if (chatPattern instanceof Array) {
+                        chatPattern.map(transformPattern)
+                    } else {
+                        chatPatterns[patternKey] = transformPattern(chatPattern)
+                    }
+                })
+            }
         })
     })
     return returnData
