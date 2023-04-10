d09a814e431671aa46d62d1f061f0c20aab95e16
KadaverBrutalo10 <jimmybiel123@gmail.com>
2023-04-10 15:16:58 +0200

    added prefixPatterns

--- advanced/prefix.js
+++ advanced/prefix.js
@@ -1,3 +1,44 @@
+module.exports = {
+    prefixPatterns: {
+        'Rainbow': 'c6eabd',
+        'Frost': 'bbff33',
+        'Lucky': '66eeff',
+        'Deutschland': '004466',
+        'Frühling': 'ffaa22',
+        'Sommer': '3be6e',
+        'Herbst': '4c6e',
+        'Winter': 'bbff',
+        'Weihnachten': '4422',
+        'Weihnachten-alt': '2244',
+        'Halloween': '66ee787',
+        'Halloween-alt': '8877e6e',
+        'Galaxy': '55ddbb33',
+        'PinkWeiss': 'ddff',
+        'WeissGrau': 'ff77',
+        'DoppelGelb': '66ee',
+        'DoppelRot': '44cc',
+        'DoppelBlau': '33bb',
+        'DoppelGrün': '22aa',
+        'DoppelGrau': '8877',
+        'DoppelLila': '55dd',
+        'Rot-weiß': ['4', 'f'],
+
+        'Weiß': 'f',
+        'Golden': '6',
+        'Rot': 'c',
+        'Magisch': 'ck',
+        'Grün': 'a',
+        'Blau': 'b',
+
+        // Team-Mitglieder Prefixe:
+        'Dunkelgrün': '2',
+        'Dunkelrot': '4',
+        'Dunkelblau': '1',
+        'Gelb': 'e',
+        'Türkis': '3'
+    }
+}
+
 /*
 [GrieferGames] Du hast dazu keine Berechtigung.
 [Prefix] Dein Prefix wurde geändert.
