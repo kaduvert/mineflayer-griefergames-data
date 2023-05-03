b59bc61e7d0a855fa6eb70dcdadbafd898bf929c
KadaverBrutalo10 <jimmybiel123@gmail.com>
2023-05-03 10:27:30 +0200

    added chatAction reference testing

---
+++ test/references.js
@@ -0,0 +1,31 @@
+const assert = require('assert')
+const { chatPatterns } = require('../basic/plot')
+
+const branches = [
+    'basic',
+    'advanced'
+]
+
+branches.forEach(branchName => {
+    const modules = require('../' + branchName)
+    Object.keys(modules).forEach(moduleName => {
+        const module = modules[moduleName]
+        describe(`${branchName}->${moduleName}`, function () {
+            const chatActions = module.chatActions
+            if (chatActions) {
+                describe(`chatActions`, function () {
+                    describe('command references', function () {
+                        Object.keys(chatActions).forEach((chatActionName) => {
+                            const chatAction = chatActions[chatActionName]
+
+                            it(chatActionName, function () {
+                                const commandName = module.commands[chatAction.command] ? chatAction.command : chatActionName
+                                assert(module.commands[commandName] !== undefined, 'has valid reference to command ' + commandName)
+                            })
+                        })
+                    })
+                })
+            }
+        })
+    })
+})
