ebc3f4a75feb9bbbf3687d22057cea7db8699e45
kaduvert <70949550+kaduvert@users.noreply.github.com>
2024-02-26 19:51:39 +0100

    update mobremover regex

--- basic/mobRemover.js
+++ basic/mobRemover.js
@@ -1,6 +1,6 @@
 module.exports = {
     chatPatterns: {
-        nextIn: /^\[MobRemover\] Achtung, in (\d+) Minuten? werden alle Tiere gelöscht\.$/,
+        nextIn: /^\[MobRemover\] Achtung! In (\d+) Minuten? werden alle Tiere gelöscht\.$/,
         removedMobs: /^\[MobRemover\] Es wurden (\d+) Tiere entfernt\.$/,
     },
     cycleTime: 20 * 60 * 1000
