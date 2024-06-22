b93da380421ee75cb87d6c6dce4068f38603dc9a
kaduvert <70949550+kaduvert@users.noreply.github.com>
2024-06-22 21:36:34 +0200

    added caseopening moneydrop regexes

--- advanced/caseOpening.js
+++ advanced/caseOpening.js
@@ -1,5 +1,7 @@
 module.exports = {
     chatPatterns: {
+        boundMoneydrop: /^\S+ ┃ (\S+) hat eine Kiste geöffnet und \S+ ┃ (\S+) hat ([\d,\.]+) Dollar erhalten!$/,
+        unboundMoneydrop: /^\S+ ┃ (\S+) hat eine Kiste geöffnet und für jeden ([\d,\.]+) Dollar gewonnen!$/,
         u: /^\[CaseOpening\] Du hast leider keine Kisten übrig. Kisten kannst du mit Kristallen kaufen. Erhalte Kristalle in unserem Webshop!$/,
         u2: /^\[CaseOpening\] Du hast EXP_BOTTLE gewonnen!$/,
     }
