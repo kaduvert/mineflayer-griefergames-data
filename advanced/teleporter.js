d4f214717a8f9424e292000bd3dca6d8f3e80ca3
kaduvert <jimmybiel123@gmail.com>
2024-07-10 11:44:47 +0200

    added teleporter public private settings

--- advanced/teleporter.js
+++ advanced/teleporter.js
@@ -23,12 +23,47 @@ module.exports = {
                 /^(.+)$/
             ],
             name: 'skull'
+        },
+        settings: {
+            display: [
+                /^Einstellungen$/
+            ]
+        },
+        public: {
+            display: [
+                /^Öffentlich$/,
+                /^Alle können den Teleporter anwählen\.$/
+            ]
+        },
+        private: {
+            display: [
+                /^Privat$/,
+                /^Nur du kannst den Teleporter anwählen\.$/
+            ]
         }
     },
     windows: {
         selectDestination: {
             titlePattern: /^GS-Teleporter$/,
-            requiredSlots: 0
+            requiredSlots: 0,
+            actions: {
+                getSettings: {
+                    itemToClick: 'settings',
+                    successEvent: 'windowOpen:settings'
+                }
+            }
+        },
+        settings: {
+            titlePattern: /^Teleporter-Einstellungen$/,
+            requiredSlots: 0,
+            actions: {
+                makePublic: {
+                    itemToClick: 'public'
+                },
+                makePrivate: {
+                    itemToClick: 'private'
+                },
+            }
         }
     }
 }
