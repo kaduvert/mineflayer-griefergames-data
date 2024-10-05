586a535cd87fb526c47f0bb3ce473cd878b9e98a
kaduvert <jimmybiel123@gmail.com>
2024-10-05 21:35:32 +0200

    add getting money with command

--- basic/money.js
+++ basic/money.js
@@ -1,5 +1,7 @@
 module.exports = {
     chatPatterns: {
+        balance: /^Kontostand: \$([\d,\.]+)$/,
+
         credited: /^\$([\d,\.]+) wurden zu deinem Konto hinzugefügt\.$/,
         transferred: /^Du hast \S+ ┃ (\S+) \$([\d,\.]+) gegeben\.$/,
         received: /^\S+ ┃ (\S+) hat dir \$([\d,\.]+) gegeben\.$/,
@@ -8,12 +10,17 @@ module.exports = {
         playerOfflineError: /^$GG Dieser Spieler wurde nicht gefunden\.$/,
     },
     commands: {
+        get: '/money',
         transfer: '/pay $1 $2'
     },
     chatActions: {
         transfer: {
             successEvent: 'transferred',
             failureEvents: ['insufficientError', 'playerOfflineError']
+        },
+        getBalanceStupidly: {
+            command: 'get',
+            successEvent: 'balance'
         }
     }
 }
